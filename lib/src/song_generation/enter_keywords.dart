945c6a8023e02da2ad9a10026e87368ba562fee0
ts3486 <taos3486@gmail.com>
2023-11-05 15:48:03 +0900

    add chip element

--- lib/src/song_generation/enter_keywords.dart
+++ lib/src/song_generation/enter_keywords.dart
@@ -32,49 +32,79 @@ class EnterKeyWordsWidgetState extends State<EnterKeyWordsWidget> {
       appBar: AppBar(
         title: const Text('Song Generator'),
       ),
-      body: SizedBox(
-          height: 230,
-          child: Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    const Text(
-                      'Enter Song Keywords',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Keyword',
-                      ),
-                      onChanged: (value) {
-                        setState(() {
-                          _keyword = value;
-                        });
-                      },
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue[300],
-                          foregroundColor: Colors.black,
-                          shape: const StadiumBorder(),
-                          minimumSize: const Size(150, 50)),
-                      onPressed: () => {_addKeyword(_keyword)},
-                      child: const Text(
-                        'Add Keyword',
+      body: Column(children: <Widget>[
+        //Input section
+        SizedBox(
+            height: 230,
+            child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      const Text(
+                        'Enter Song Keywords',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          color: Colors.black,
+                          color: Colors.white,
+                          fontSize: 20,
                         ),
                       ),
-                    ),
-                  ]))),
+                      TextField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Keyword',
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            _keyword = value;
+                          });
+                        },
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.orange[300],
+                            foregroundColor: Colors.black,
+                            shape: const StadiumBorder(),
+                            minimumSize: const Size(150, 50)),
+                        onPressed: () => {_addKeyword(_keyword)},
+                        child: const Text(
+                          'Add Keyword',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ]))),
+        //Display Section
+        SizedBox(
+            height: 230,
+            child: Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      Chip(
+                        labelPadding: const EdgeInsets.all(2.5),
+                        // avatar: CircleAvatar(
+                        //   backgroundColor: Colors.white70,
+                        //   child: Text(_keywords[0].toUpperCase()),
+                        // ),
+                        label: const Text(
+                          'keyword1',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        backgroundColor: Colors.blue[300],
+                        elevation: 6.0,
+                        shadowColor: Colors.grey[60],
+                        deleteIcon: const Icon(Icons.close),
+                        onDeleted: () {},
+                      ),
+                    ]))),
+      ]),
 
       // floatingActionButton: FloatingActionButton(
       //   onPressed: _addKeyword(_keyword),
